b2f48fe0d28a2a4dacb83eede23552b2fe6c82e1
JonMarcos <60173994+JonMarcos@users.noreply.github.com>
2022-02-22 05:02:55 +0100

    Update constants.py

--- constants.py
+++ constants.py
@@ -2,6 +2,6 @@
 
 
 #Images
-LOCK = "C:/Users/Jon/github/PasswordLocalManager/lock.png"
-LOCK_ICO = "C:/Users/Jon/github/PasswordLocalManager/lock.ico"
-OPEN_LOCK = "C:/Users/Jon/github/PasswordLocalManager/open_lock.png"
+LOCK = "C:/Users/Jon/github/PasswordLocalManager/Images/lock.png"
+LOCK_ICO = "C:/Users/Jon/github/PasswordLocalManager/Images/lock.ico"
+OPEN_LOCK = "C:/Users/Jon/github/PasswordLocalManager/Images/open_lock.png"
